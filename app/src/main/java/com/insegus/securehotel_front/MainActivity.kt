da54b01df66e6ffbd799e978278ae1fd4b51ed53
Alejandro Santiago Félix <alex.0002002@gmail.com>
2024-05-08 12:25:26 +0200

    feat: Data processing

--- app/src/main/java/com/insegus/securehotel_front/MainActivity.kt
+++ app/src/main/java/com/insegus/securehotel_front/MainActivity.kt
@@ -1,24 +1,26 @@
 package com.insegus.securehotel_front
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.insegus.securehotel_front.data.model.ClientPetition
 import com.insegus.securehotel_front.data.model.Material
 import com.insegus.securehotel_front.ui.components.MaterialList
 import com.insegus.securehotel_front.ui.components.PetitionButton
 import com.insegus.securehotel_front.ui.components.Title
 import com.insegus.securehotel_front.ui.theme.SecureHotelFRONTTheme
+import java.text.DateFormat
+import java.util.Date
 
 class MainActivity : ComponentActivity() {
 
@@ -53,8 +55,22 @@ class MainActivity : ComponentActivity() {
                     PetitionButton(
                         onConfirm = {
 
+                            val selectedMaterials = materials.filter { it.isSelected }
+                            val currentDateTime = DateFormat.getDateTimeInstance().format(Date())
+                            val clientPetitions = selectedMaterials.map { material ->
+                                ClientPetition(
+                                    clientId = it,
+                                    nameMaterial = material.name,
+                                    amount = material.amount.toInt(),
+                                    digitalSignature = "YourDigitalSignatureHere",
+                                    orderDate = currentDateTime
+                                )
+                            }
+
+                            Log.d("Petitions", clientPetitions.toString())
                         }
                     )
+
                 }
             }
         }
